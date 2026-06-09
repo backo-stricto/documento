7d94aeb2950643d42e938cbbab8578357fbca489
Théo BL <theo.biasutto-lervat@inria.fr>
2026-06-09 16:53:55 +0200

    wip: using new _meta from backo

--- src/documento/openapi.py
+++ src/documento/openapi.py
@@ -11,11 +11,70 @@ def create_openapi_spec(backo_meta: dict):
             "version": backo_meta.get("version", ""),
         },
         "paths": {},
-        "components": {"schemas": {}},
+        "components": {
+            "schemas": {
+                "json-patch": {
+                    "type": "array",
+                    "items": {
+                        "oneOf": [
+                            {
+                                "additionalProperties": False,
+                                "required": ["value", "op", "path"],
+                                "properties": {
+                                    "path": {
+                                        "description": "A JSON Pointer path.",
+                                        "type": "string",
+                                    },
+                                    "op": {
+                                        "description": "The operation to perform.",
+                                        "type": "string",
+                                        "enum": ["add", "replace", "test"],
+                                    },
+                                    "value": {
+                                        "description": "The value to add, replace or test."
+                                    },
+                                },
+                            },
+                            {
+                                "additionalProperties": False,
+                                "required": ["op", "path"],
+                                "properties": {
+                                    "path": {
+                                        "description": "A JSON Pointer path.",
+                                        "type": "string",
+                                    },
+                                    "op": {
+                                        "description": "The operation to perform.",
+                                        "type": "string",
+                                        "enum": ["remove"],
+                                    },
+                                },
+                            },
+                            {
+                                "additionalProperties": False,
+                                "required": ["from", "op", "path"],
+                                "properties": {
+                                    "path": {
+                                        "description": "A JSON Pointer path.",
+                                        "type": "string",
+                                    },
+                                    "op": {
+                                        "description": "The operation to perform.",
+                                        "type": "string",
+                                        "enum": ["move", "copy"],
+                                    },
+                                },
+                            },
+                        ]
+                    },
+                }
+            }
+        },
     }
     for schema in extract_schemas(backo_meta):
         print(f"Adding schema {schema.title}")
         openapi["components"]["schemas"][schema.title] = asdict(schema)
+
     for route in extract_routes(backo_meta):
         print(f"Adding route {route.path}")
         openapi["paths"][route.path] = {}
@@ -26,11 +85,17 @@ def create_openapi_spec(backo_meta: dict):
                 "operationId": method.operationId,
             }
             if len(method.parameters) > 0:
-                openapi["paths"][route.path][method.mode]["parameters"] = method.parameters
+                openapi["paths"][route.path][method.mode]["parameters"] = (
+                    method.parameters
+                )
             if len(method.requestBody) > 0:
-                openapi["paths"][route.path][method.mode]["requestBody"] = method.requestBody
+                openapi["paths"][route.path][method.mode]["requestBody"] = (
+                    method.requestBody
+                )
             if len(method.responses) > 0:
-                openapi["paths"][route.path][method.mode]["responses"] = method.responses
+                openapi["paths"][route.path][method.mode]["responses"] = (
+                    method.responses
+                )
     return openapi
 
 
@@ -87,7 +152,7 @@ def extract_schemas(backo_meta: dict) -> Iterator[Schema]:
             description=collection.get("description", ""),
         )
         schema_with_id = Schema(
-            title=f"{collection.get("name", "")}_identified",
+            title=f"{collection.get('name', '')}_identified",
             description=collection.get("description", ""),
         )
         for name, infos in collection["item"]["sub_scheme"].items():
@@ -95,7 +160,9 @@ def extract_schemas(backo_meta: dict) -> Iterator[Schema]:
             if name == "_meta":
                 continue
             elif name == "_id":
-                schema_with_id.add_property(name, "string", infos.get("description", ""))
+                schema_with_id.add_property(
+                    name, "string", infos.get("description", "")
+                )
             else:
                 type = "null"
                 if "String" in infos["types"]:
@@ -123,170 +190,50 @@ def extract_schemas(backo_meta: dict) -> Iterator[Schema]:
 
 def extract_routes(backo_meta: dict) -> Iterator[Route]:
     for collection in backo_meta.get("collections", []):
-        route = Route(path=f"/{collection['name']}")
+        routes: dict[str, Route] = {}
 
-        if collection["rights"]["read"]:
-            get = Method(
-                mode="get",
-                operationId=f"get_{collection['name']}",
-                summary=f"read {collection['name']} collection",
-            )
-            get.add_response(
-                "200",
-                f"Read whole list of {collection['name']}",
-                {
-                    "application/json": {
-                        "schema": {
-                            "title": f"Response list of {collection['name']}",
-                            "type": "array",
-                            "items": {
-                                "$ref": f"#/components/schemas/{collection['name']}_identified"
-                            },
-                        }
-                    }
-                },
-            )
-            route.add_method(get)
+        for route in collection["routes"]:
+            if route["url"] not in routes:
+                routes[route["url"]] = Route(path=route["url"])
 
-        if collection["rights"]["create"]:
-            post = Method(
-                mode="post",
-                operationId=f"post_{collection['name']}",
-                summary=f"create object in {collection['name']} collection",
+            method = Method(
+                mode=route["method"].lower(),
+                operationId=f"{route['method'].lower()}_{route['url']}",
+                summary=route["description"],
             )
-            post.add_request_body(
-                {
-                    "application/json": {
-                        "schema": {
-                            "$ref": f"#/components/schemas/{collection['name']}"
-                        }
-                    }
-                },
-            )
-            route.add_method(post)
+            if route["parameters"]:
+                for param in route["parameters"]:
+                    match param["mode"]:
+                        case "string-query":
+                            method.add_parameter(
+                                {
+                                    "name": param["name"],
+                                    "in": "query",
+                                    "required": False,
+                                    "schema": {"type": "string"},
+                                    "description": "Query string",
+                                }
+                            )
+                        case "path":
+                            method.add_parameter(
+                                {
+                                    "name": param["name"],
+                                    "in": "path",
+                                    "required": True,
+                                    "schema": {"type": "string"},
+                                }
+                            )
+                        case "header":
+                            pass
+                        case "cookie":
+                            pass
+            if route["requestBody"]:
+                for body in route["requestBody"]:
+                    ...  # TODO
+            if route["responses"]:
+                for resp in route["responses"]:
+                    ...  # TODO
 
-        yield route
+            routes[route["url"]].add_method(method)
 
-        route = Route(path=f"/{collection['name']}/_meta")
-        route.add_method(
-            Method(
-                mode="get",
-                operationId=f"meta_{collection['name']}",
-                summary=f"get metadata for {collection['name']} collection",
-            )
-        )
-        yield route
-
-        route = Route(path=f"/{collection['name']}/_check")
-        post = Method(
-            mode="post",
-            operationId=f"check_{collection['name']}",
-            summary=f"check {collection['name']} collection",
-        )
-        post.add_request_body(
-            {
-                "application/json": {
-                    "schema": {
-                        "$ref": f"#/components/schemas/{collection['name']}"
-                    }
-                }
-            },
-        )
-        route.add_method(post)
-        yield route
-
-        route = Route(path=f"/{collection['name']}/{{_id}}")
-        if collection["rights"]["create"]:
-            post = Method(
-                mode="post",
-                operationId=f"post_{collection['name']}",
-                summary=f"create object in {collection['name']} collection",
-            )
-            post.add_parameter(
-                {
-                    "name": "_id",
-                    "in": "path",
-                    "required": True,
-                    "schema": {"type": "string"},
-                }
-            )
-            post.add_request_body(
-                {
-                    "application/json": {
-                        "schema": {
-                            "$ref": f"#/components/schemas/{collection['name']}"
-                        }
-                    }
-                }
-            )
-            route.add_method(post)
-
-        if collection["rights"]["read"]:
-            get = Method(
-                mode="get",
-                operationId=f"get_{collection['name']}",
-                summary=f"get object with id {{_id}} in {collection['name']} collection",
-            )
-            get.add_parameter(
-                {
-                    "name": "_id",
-                    "in": "path",
-                    "required": True,
-                    "schema": {"type": "string"},
-                }
-            )
-            route.add_method(get)
-
-        if collection["rights"]["modify"]:
-            put = Method(
-                mode="put",
-                operationId=f"put_{collection['name']}",
-                summary=f"update object with id {{_id}} in {collection['name']} collection",
-            )
-            put.add_parameter(
-                {
-                    "name": "_id",
-                    "in": "path",
-                    "required": True,
-                    "description": f"id of the {collection['name']} object to update",
-                    "schema": {"type": "string"},
-                }
-            )
-            route.add_method(put)
-
-            patch = Method(
-                mode="patch",
-                operationId=f"patch_{collection['name']}",
-                summary=f"update object with id {{_id}} in {collection['name']} collection",
-            )
-            patch.add_parameter(
-                {
-                    "name": "_id",
-                    "in": "path",
-                    "required": True,
-                    "description": f"id of the {collection['name']} object to patch",
-                    "schema": {"type": "string"},
-                }
-            )
-            route.add_method(patch)
-
-        if collection["rights"]["delete"]:
-            delete = Method(
-                mode="delete",
-                operationId=f"delete_{collection['name']}",
-                summary=f"delete object with id {{_id}} in {collection['name']} collection",
-            )
-            delete.add_parameter(
-                {
-                    "name": "_id",
-                    "in": "path",
-                    "required": True,
-                    "description": f"id of the {collection['name']} object to delete",
-                    "schema": {"type": "string"},
-                }
-            )
-            route.add_method(delete)
-
-        yield route
-
-        # TODO route action, route view
+        yield from routes.values()
